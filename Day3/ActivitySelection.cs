f7c66778ee5938b4ee1f33680c6a28166f0d46f1
M1kuAr1us <ivan.zavivan@gmail.com>
2025-05-23 02:38:47 +0300

    Add Day Three Tasks

---
+++ Day3/ActivitySelection.cs
@@ -0,0 +1,146 @@
+﻿namespace Day3;
+
+public class ActivitySelection
+{
+    public static void GreedyRecursiveActivityMain()
+    {
+        int[] start = { 1, 3, 0, 5, 8, 5 };
+        int[] finish = { 2, 4, 6, 7, 9, 9 };
+        int n = start.Length;
+        
+        int[] indices = new int[n];
+        for (int i = 0; i < n; i++) indices[i] = i;
+        HeapSortByFinishTime(indices, finish);
+        
+        int[] s = new int[n + 1];
+        int[] f = new int[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            s[i + 1] = start[indices[i]];
+            f[i + 1] = finish[indices[i]];
+        }
+
+        Console.WriteLine("Selected activities (greedy approach):");
+        RecursiveActivitySelector(s, f, 0, n);
+    }
+    
+    public static void DynamicActivityMain()
+    {
+        int[] start = { 1, 3, 0, 5, 8, 5 };
+        int[] finish = { 2, 4, 6, 7, 9, 9 };
+        int n = start.Length;
+        int[] indices = new int[n];
+        for (int i = 0; i < n; i++) indices[i] = i;
+        
+        HeapSortByFinishTime(indices, finish);
+        
+        int[] s = new int[n + 2];
+        int[] f = new int[n + 2];
+        for (int i = 0; i < n; i++)
+        {
+            s[i + 1] = start[indices[i]];
+            f[i + 1] = finish[indices[i]];
+        }
+
+        s[0] = f[0] = 0;
+        s[n + 1] = f[n + 1] = int.MaxValue;
+
+        int[,] c = new int[n + 2, n + 2];
+        int[,] act = new int[n + 2, n + 2];
+
+        DynamicActivitySelector(s, f, n, c, act);
+        Console.WriteLine("The set contains:");
+        PrintActivities(c, act, 0, n + 1);
+    }
+    
+    static void RecursiveActivitySelector(int[] s, int[] f, int k, int n)
+    {
+        int m = k + 1;
+        while (m <= n && s[m] < f[k])
+        {
+            m++;
+        }
+        if (m <= n)
+        {
+            Console.WriteLine("Activity " + m);
+            RecursiveActivitySelector(s, f, m, n);
+        }
+    }
+    
+    static void DynamicActivitySelector(int[] s, int[] f, int n, int[,] c, int[,] act)
+    {
+        for (int l = 2; l <= n + 1; l++)
+        {
+            for (int i = 0; i <= n + 1 - l; i++)
+            {
+                int j = i + l;
+                c[i, j] = 0;
+                for (int k = j - 1; k > i; k--)
+                {
+                    if (f[i] <= s[k] && f[k] <= s[j])
+                    {
+                        int q = c[i, k] + c[k, j] + 1;
+                        if (q > c[i, j])
+                        {
+                            c[i, j] = q;
+                            act[i, j] = k;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("A maximum size set of mutually compatible activities has size " + c[0, n + 1]);
+    }
+
+    static void PrintActivities(int[,] c, int[,] act, int i, int j)
+    {
+        if (c[i, j] > 0)
+        {
+            int k = act[i, j];
+            Console.WriteLine("Activity " + k);
+            PrintActivities(c, act, i, k);
+            PrintActivities(c, act, k, j);
+        }
+    }
+
+    static void HeapSortByFinishTime(int[] indices, int[] finish)
+    {
+        int heapSize = indices.Length;
+        BuildMaxHeap(indices, finish, heapSize);
+
+        for (int i = indices.Length - 1; i >= 1; i--)
+        {
+            (indices[0], indices[i]) = (indices[i], indices[0]);
+            heapSize--;
+            MaxHeapify(indices, finish, 0, heapSize);
+        }
+    }
+
+    static void BuildMaxHeap(int[] indices, int[] finish, int heapSize)
+    {
+        for (int i = heapSize / 2 - 1; i >= 0; i--)
+        {
+            MaxHeapify(indices, finish, i, heapSize);
+        }
+    }
+
+    static void MaxHeapify(int[] indices, int[] finish, int i, int heapSize)
+    {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+
+        if (left < heapSize && finish[indices[left]] > finish[indices[largest]])
+            largest = left;
+
+        if (right < heapSize && finish[indices[right]] > finish[indices[largest]])
+            largest = right;
+
+        if (largest != i)
+        {
+            (indices[i], indices[largest]) = (indices[largest], indices[i]);
+            MaxHeapify(indices, finish, largest, heapSize);
+        }
+    }
+}
---
+++ Day3/KnapsackProblem.cs
@@ -0,0 +1,90 @@
+﻿namespace Day3;
+
+public class KnapsackProblem
+{
+    public static void KnapsackDynamicMain()
+    {
+        int W = 10;
+        int[] values = [10, 20, 20, 40, 60];
+        int[] weights = [1, 1, 3, 2, 5];
+        int n = values.Length;
+
+        int[,] M = new int[n + 1, W + 1];
+        
+        for (int i = 1; i <= n; i++)
+        {
+            for (int w = 1; w <= W; w++)
+            {
+                if (weights[i - 1] <= w)
+                {
+                    M[i, w] = Math.Max(M[i - 1, w],
+                        M[i - 1, w - weights[i - 1]] + values[i - 1]);
+                }
+                else
+                {
+                    M[i, w] = M[i - 1, w];
+                }
+            }
+        }
+        
+        Console.WriteLine("Maximum value: " + M[n, W]);
+        
+        Console.WriteLine("Selected items (indexes from 1):");
+        int remainingWeight = W;
+        for (int i = n; i > 0 && remainingWeight > 0; i--)
+        {
+            if (M[i, remainingWeight] != M[i - 1, remainingWeight])
+            {
+                Console.WriteLine($"Item {i} (price: {values[i - 1]}, weight: {weights[i - 1]})");
+                remainingWeight -= weights[i - 1];
+            }
+        }
+    }
+    
+    public static void KnapsackGreedyMain()
+    {
+        int[] values = [10, 20, 20, 40, 60];
+        int[] weights = [1, 1, 3, 2, 5];
+        int W = 10;
+        int totalValue = 0;
+
+        int n = values.Length;
+        double[] ratio = new double[n];
+        int[] indices = new int[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            ratio[i] = (double)values[i] / weights[i];
+            indices[i] = i;
+        }
+        
+        Array.Sort(indices, (a, b) => ratio[b].CompareTo(ratio[a]));
+
+        int[] solution = new int[n];
+        int cumWeight = 0;
+        
+        foreach (int i in indices)
+        {
+            if (cumWeight + weights[i] <= W)
+            {
+                solution[i] = 1;
+                cumWeight += weights[i];
+            }
+            else
+            {
+                solution[i] = 0;
+            }
+        }
+        
+        Console.WriteLine("Included items:");
+        for (int i = 0; i < n; i++)
+        {
+            if (solution[i] == 1)
+            {
+                Console.WriteLine($"Item {i + 1} (price: {values[i]}, weight: {weights[i]})");
+                totalValue += values[i];
+            }
+        }
+        Console.WriteLine($"Total Value: {totalValue}");
+    }
+}
