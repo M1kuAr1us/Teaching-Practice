7c2fd29770d35811831b55327c0b1e8026424b13
M1kuAr1us <ivan.zavivan@gmail.com>
2025-06-01 20:29:31 +0300

    IDE Hot Fix

--- Day2/obj/Debug/net9.0/Day2.AssemblyInfo.cs
+++ Day2/obj/Debug/net9.0/Day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b58846deca3e4b1d0d4779fd4f4137af731f1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bada4bf6425460cf160569399d4e7a3e2c877fd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
